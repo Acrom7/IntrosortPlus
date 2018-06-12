dd8e1f3bf8739ce32107745410dc36456db57382
Acrom7 <yadima19988@gmail.com>
2018-06-12 18:41:58 +1000

    Рабочий FileChooser (только *.seq, в папке resources/seq)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,48 +1,68 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 
 import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.util.List;
 
 
 public class Controller {
 
-    @FXML public RadioButton rbDecType;
-    @FXML public RadioButton rbIncType;
-    @FXML public RadioButton rbConstType;
-    @FXML public ToggleGroup kindOfFiles;
-    @FXML public TextField txtConstValue;
-    @FXML public TextField txtNumOfFiles;
+    public Button btnSort;
+    //Группа для выбора типа файлов
+    @FXML private ToggleGroup kindOfFiles;
+    @FXML private RadioButton rbRandomSeq;
+    @FXML private RadioButton rbAPSeq;
+    @FXML private RadioButton rbKillerSeq;
 
+    //Текстовые поля вкладки "Генерация"
+    @FXML private TextField txtFirstValue;
+    @FXML private TextField txtStepSeq;
+    @FXML private TextField txtNumOfFiles;
+    @FXML private TextField txtNumOfElem;
 
-    public void addFiles(ActionEvent actionEvent) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Files");
-        fileChooser.showOpenDialog(((Node)actionEvent.getSource()).getScene().getWindow());
+    //Таблица файлов
+    @FXML private TableColumn<MyFile, String> fileName;
+    @FXML private TableColumn<MyFile, CheckBox> select;
+    @FXML private TableView<MyFile> tvFiles;
+    private ObservableList<MyFile> myFiles = FXCollections.observableArrayList();
+
+    //Графики
+    @FXML private BarChart bcTime;
+    @FXML private BarChart bcComparisons;
+    @FXML private BarChart bcExchanges;
+
+    // Включаем/Отключаем доступность полей txtFirstValue && txtStepSeq
+    public void chooseAPSeq(ActionEvent actionEvent) {
+        txtFirstValue.setDisable(false);
+        txtStepSeq.setDisable(false);
     }
-
-    // Включаем/Отключаем доступность поля constValue
-    public void chooseConstType(ActionEvent actionEvent) {
-        rbConstType.setUserData("Константа");
-        txtConstValue.setDisable(false);
+    public void chooseRandomSeq(ActionEvent actionEvent) {
+        txtFirstValue.setDisable(true);
+        txtStepSeq.setDisable(true);
     }
-
-    public void chooseDecType(ActionEvent actionEvent) {
-        rbDecType.setUserData("Убывающая");
-        txtConstValue.setDisable(true);
-    }
-
-    public void chooseIncType(ActionEvent actionEvent) {
-        rbIncType.setUserData("Возрастающая");
-        txtConstValue.setDisable(true);
+    public void chooseKillerSeq(ActionEvent actionEvent) {
+        txtFirstValue.setDisable(true);
+        txtStepSeq.setDisable(true);
     }
 
     //Генерация файлов для сортировка
     public void btnGenerateFiles(ActionEvent actionEvent) {
+        rbAPSeq.setUserData("APSeq");
+        rbKillerSeq.setUserData("KillerSeq");
+        rbRandomSeq.setUserData("RandomSeq");
+
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Информация");
         alert.setHeaderText("Ваш выбор");
@@ -52,5 +72,45 @@ public class Controller {
         s += "Количество: " + txtNumOfFiles.getText();
         alert.setContentText(s);
         alert.show();
+    }
+
+    //Добавление файлов для сортировки
+    public void addFiles(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Files");
+        fileChooser.setInitialDirectory(new File("resources/seq"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("SEQ files (*.seq)","*.seq"));
+        List<File> files = fileChooser.showOpenMultipleDialog(((Node)actionEvent.getSource()).getScene().getWindow());
+
+        if (files != null) {
+            for (int i = 0; i < files.size(); i++) {
+                myFiles.add(new MyFile(files.get(i)));
+            }
+            select.setStyle("-fx-alignment: BASELINE_CENTER;");
+            tvFiles.setItems(myFiles);
+            fileName.setCellValueFactory(new PropertyValueFactory<MyFile, String>("name"));
+            select.setCellValueFactory(new PropertyValueFactory<MyFile, CheckBox>("checkBox"));
+        }
+    }
+
+   @FXML
+    public void testDrawChart() {
+        final String austria = "Austria";
+        final String brazil = "Brazil";
+        final String france = "France";
+        final String italy = "Italy";
+        final String usa = "USA";
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("2004");
+        series2.getData().add(new XYChart.Data(austria, 57401.85));
+        series2.getData().add(new XYChart.Data(brazil, 41941.19));
+        series2.getData().add(new XYChart.Data(france, 45263.37));
+        series2.getData().add(new XYChart.Data(italy, 117320.16));
+        series2.getData().add(new XYChart.Data(usa, 14845.27));
+        bcTime.getData().addAll(series2);
+    }
+
+    public void sortFiles(ActionEvent actionEvent) {
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,7 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("GUI for Introsort-plus");
-        primaryStage.setScene(new Scene(root, 630, 400));
+        primaryStage.setScene(new Scene(root, 636, 400));
+        primaryStage.setMinHeight(root.minHeight(-1));
+        primaryStage.setMinWidth(root.minWidth(-1));
         primaryStage.show();
     }
 
